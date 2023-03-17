6e36ce4335cd45d8d590099606125f6378cef831
Lawrence Jordan <lw342864@dal.ca>
2023-03-17 12:42:28 -0300

    Refactor name of admin page to scenes page

--- routes/scenes.js
+++ routes/scenes.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   let numScenes = fs.readdirSync("./public/scenes/");
   let scenes = JSON.parse(fs.readFileSync("./public/scenes/scenes.json", "utf8"));
   
-  res.render('admin', {title: 'Admin Page', scenes: scenes, numScenes : numScenes});
+  res.render('scenes', {title: 'Scene Manager', scenes: scenes, numScenes : numScenes});
 });
 
 module.exports = router;
