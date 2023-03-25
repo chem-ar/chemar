fac0e1ce820aa16685d2a11ca9079ba650445c1f
Liam Osler <osler.liam@gmail.com>
2023-03-25 02:45:22 -0300

    sceneViewer routing and view creation

---
+++ routes/sceneViewer.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs'); 
+
+router.get('/', function(req, res, next) {
+  res.render('sceneViewer', { title: 'Scene Viewer', item: 2519 });
+});
+
+
+router.get('/:id', function(req , res){
+  var scenefiles = fs.readdirSync('./public/scenefiles/')
+
+  if(scenefiles.includes(req.params.id)){
+    res.render('sceneViewer', {
+      title: 'Scene Viewer', 
+      item: req.params.id
+    });
+  }
+  
+  else{
+    res.render('sceneViewer', {
+      title: 'Scene Viewer', 
+      item: 2519
+    });  
+  }
+
+});
+
+module.exports = router;
