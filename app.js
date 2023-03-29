fd4899c062cad2c983c70e9b3f99011c5d7f8b92
Lawrence Jordan <lw342864@dal.ca>
2023-03-29 10:06:28 -0300

    Added cookie secret and navbarAdmin Test

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.set("view engine", "ejs");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser("iauuhdfsoivfdsoviufh"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
