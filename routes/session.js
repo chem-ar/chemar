747590c6f96e1a38ca8c6647b7c965103558c595
Lawrence Jordan <lw342864@dal.ca>
2023-03-29 13:02:03 -0300

    Almost Working

--- routes/session.js
+++ routes/session.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 //Create cookie here then redirect
 router.get('/', function(req, res, next) {
-  const oneDay = 86400000
-  res.cookie('admin', true, {maxAge: oneDay, signed: true});
   res.redirect("/");
 });
 
+router.post('/', function(req, res, next) {
+  let adminPass = JSON.parse(fs.readFileSync("./admin.json")).admin.password;
+  console.log(adminPass);
+  if(adminPass == req.body.JSON){
+    const oneDay = 86400000
+    res.cookie('admin', true, {maxAge: oneDay, signed: true});
+    res.redirect("/");
+  }
+  else{
+    res.redirect("back");
+  }
+});
 module.exports = router;
