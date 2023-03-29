7f9cbd4c991713c8064a105539244cb06be422e2
Lawrence Jordan <lw342864@dal.ca>
2023-03-29 10:06:28 -0300

    Added cookie secret and navbarAdmin Test

--- routes/session.js
+++ routes/session.js
@@ -4,7 +4,7 @@ var router = express.Router();
 //Create cookie here then redirect
 router.get('/', function(req, res, next) {
   const oneDay = 86400000
-  res.cookie('name', 'admin', {maxAge: oneDay});
+  res.cookie('admin', true, {maxAge: oneDay, signed: true});
   res.redirect("/");
 });
 
