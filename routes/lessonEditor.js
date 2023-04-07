d9fab6414c23396f63e2ac9ba3b875980997fda1
Liam Osler <osler.liam@gmail.com>
2023-04-07 02:37:28 -0300

    add lessoneditor
    Co-authored-by: sehljacob <sehljacob@users.noreply.github.com>
    Co-authored-by: Geoffrey Belcher <yeoffrey@users.noreply.github.com>
    Co-authored-by: Brent Stevens <br477448@dal.ca>

--- routes/lessonEditor.js
+++ routes/lessonEditor.js
@@ -1,1 +1,26 @@
-Hello
+var express = require('express');
+var router = express.Router();
+var fs = require('fs'); 
+
+router.get('/', function(req, res, next) {
+  res.render('lessonEditor', { title: 'Scene Viewer'});
+});
+
+
+router.get('/:id', function(req , res){
+  var lessonfiles = fs.readdirSync('./public/lessons/')
+
+  if(lessonfiles.includes(req.params.id)){
+    res.render('lessonEditor', {
+      title: 'Scene Viewer', 
+      item: req.params.id
+    });
+  }
+  
+  else{
+    res.render('error', { title: 'MoleculAR - Error', message: 'Lesson not found', error: {status: 404, stack: 'Scene not found'}});
+  }
+
+});
+
+module.exports = router;
