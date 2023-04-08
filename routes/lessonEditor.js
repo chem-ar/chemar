e0e5d7e4ce071186769e6c9d2f6a2da9147ba822
Liam Osler <osler.liam@gmail.com>
2023-04-07 23:28:33 -0300

    add image upload route
    Co-authored-by: Geoffrey Belcher <yeoffrey@users.noreply.github.com>

--- routes/lessonEditor.js
+++ routes/lessonEditor.js
@@ -20,7 +20,32 @@ router.get('/:id', function(req , res){
   else{
     res.render('error', { title: 'MoleculAR - Error', message: 'Lesson not found', error: {status: 404, stack: 'Scene not found'}});
   }
-
 });
+
+router.post('/upload/images', function(req , res){
+  console.log(req.body);
+  let image = req.body["image-contents"].replace(/^data:image\/png;base64,/, "");
+
+  console.log(req.body["image-name"]);
+
+  const newImage = './public/images/' + req.body["image-name"];
+
+  fs.writeFile(newImage, image, 'base64', function(err) {
+    console.log(err);
+  });
+
+  // res.status(200).send("File uploaded successfully.");
+  
+  res.status(200).json(
+    {
+      "status": "success",
+      "message": "File uploaded successfully.",
+      "file-src": newImage.replace('./public', '')
+    }
+  );
+
+  
+});
+
 
 module.exports = router;
