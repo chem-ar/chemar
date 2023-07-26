df50d6d42fb9ad1ef4b3f61397a0d7ee10b9cdaf
unknown <kr954026@dal.ca>
2023-07-26 12:27:48 -0300

    Fixed Admin navbar error.(working navbar for both user and admin)

--- routes/about.js
+++ routes/about.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const router= express.Router()
+var express = require('express');
+var router = express.Router();
 
-/* GET about page. */
+// Get about page
 router.get('/', function(req, res, next) {
-    //Admin check
-    let isAdmin = (req.signedCookies.admin == 'true')
-    
-    res.render('about', { title: 'Express', testJSON: {name: 'test'}, isAdmin: isAdmin});
+
+  //Admin check
+  let isAdmin = (req.signedCookies.admin == 'true')
+
+  res.render('about', { title: 'Express', testJSON: {name: 'test'}, isAdmin: isAdmin});
 });
-  
+
 module.exports = router;
+
   
