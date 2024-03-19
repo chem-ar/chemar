eaa454198db4c6483b2ef01dd1f59d25f2bb8030
Oluwatoni Adeniyi <ol618725@dal.ca>
2024-03-19 13:05:44 -0300

    removed commented out code

--- routes/models.js
+++ routes/models.js
@@ -3,45 +3,10 @@ var router = express.Router();
 
 /* GET models page. */
 router.get('/', function(req, res, next) {
-    // const models = './public/models1/';
-
     //Admin check
     let isAdmin = (req.signedCookies.admin == 'true');
 
     res.render('models', { title: 'Model Catalog', isAdmin: isAdmin});
 });
-
-// router.get('/', function(req, res, next) {
-//     const molfiles = './public/molfiles/';
-
-//     //Admin check
-//     let isAdmin = (req.signedCookies.admin == 'true');
-
-
-//     let listItems =  fs.readdirSync(molfiles);
-//     let molfileJSON = fs.readFileSync('./public/catalog/molfileCatalog.json', 'utf8');
-//     let molfileObject = JSON.parse(molfileJSON);
-    
-
-//     let finalList = [];
-
-//     for(let item of listItems){
-
-//         molfileObject[item].file = item;
-//         finalList.push(molfileObject[item])
-//     }
-//     console.log(finalList)
-
-//     res.render('molecules', { title: 'Catalog', list: finalList, isAdmin: isAdmin});
-// });
-
-// router.get('/list', function(req, res) {
-//     const models = './public/models1/';
-
-//     //Admin check
-//     let isAdmin = (req.signedCookies.admin == 'true');
-
-//     res.status(200).send(fs.readdirSync(models));
-// });
 
 module.exports = router;
