5bad32cafe7432f75743eeab9977d291c638e508
Skyler <sk498216@dal.ca>
2024-04-02 07:50:08 -0300

    Adding comment for unfinished work

--- routes/addModel.js
+++ routes/addModel.js
@@ -8,17 +8,17 @@ router.get('/', function(req, res, next) {
     res.render('addModel', {title: 'Add New Model'});
 });
 
-router.post('/saveModel', async (req, res) => {
+//This endpoint needs to be completed to handle the uploading for the files to public/modelfiles
+router.post('/saveModel', upload.array('modelFiles', 5), async (req, res) => {
     // Assign values from form to variables
-    var fileName = req.body.name;
-    console.log(fileName);
-    var files = req.body.fileName;
-    console.log(files);
+    var name = req.body.name;
+    console.log(name);
+    
     var modelDescription = req.body.modelDescription;
     console.log(modelDescription);
     
     var models = fs.readdirSync('./public/modelfiles/');
-    var fileIsPresent = models.includes(fileName);
+    var fileIsPresent = models.includes(modelFiles);
 
     // Check if the molecule already exists
     if (fileIsPresent) {
@@ -30,9 +30,23 @@ router.post('/saveModel', async (req, res) => {
         // Redirect back to the catalog page
         return res.redirect('/models');
     }
-        for (let i = 0; i < files.length; i++){
-            console.log(i);
+    for (let i = 0; i < modelFiles.length; i++) {
+        const file = modelFiles[i];
+        const fileName = file.filename;
+        const filePath = `public/modelfiles/${fileName}`;
+
+        // Check if the file already exists
+        if (fs.existsSync(filePath)) {
+            console.log(`File ${fileName} already exists.`);
+            notifier.notify({
+                title: 'Save Unsuccessful.',
+                message: `Cannot save file ${fileName}, it already exists.`,
+            });
+            // Redirect back to the catalog page
+            return res.redirect('/models');
         }
+    }
+
 
         // Read modelFileCatalog json file and add info to it
         var rawdata = fs.readFileSync('./public/catalog/modelFileCatalog.json');
@@ -42,10 +56,10 @@ router.post('/saveModel', async (req, res) => {
         
         var modelName = req.body.name;
         
-        parsedData[fileName] = {
+        parsedData[modelName] = {
             name: modelName,
             description: modelDescription,
-            files: files
+            modelFiles: modelFiles.map(file => file.filename)
         };
 
         var newModel = JSON.stringify(parsedData);
