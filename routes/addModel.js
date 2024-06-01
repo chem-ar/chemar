06dfad66e22ab6c450e57fdd02f5913f6166a6b6
Dhruvil Pechiwala <dhruvilpechiwala@Dhruvils-MacBook-Air.local>
2024-06-01 18:24:11 -0300

    Introducing the path constant

--- routes/addModel.js
+++ routes/addModel.js
@@ -3,44 +3,53 @@ var express = require('express');
 var multer = require('multer');
 var router = express.Router();
 var fs = require('fs');
+const path = require('path');//new
 const notifier = require('node-notifier'); // Node Notifiers: https://www.npmjs.com/package/node-notifier
 
 // Set up multer to handle file uploads
 const storage = multer.diskStorage({
-    destination: (req, res, cb) => {
-        cb(null, 'public/modelfiles')
+    destination: (req, file, cb) => {//new
+        cb(null, 'public/modelfiles/admin')
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + '-' + file.originalname);
     }
 })
 const upload = multer({
-    dest: storage
+    storage: storage
 });
 
-const uploadMiddleware = (req, res, next) => {
+router.get('/', function(req, res, next) {
+    res.render('addModel', {title: 'Add New Model'});
+});
+
+const uploadMiddleware = upload.fields([
+    { name: 'objFileName', maxCount: 1 },
+    { name: 'mtlFileName', maxCount: 1 }
+]);
+/*const uploadMiddleware = (req, res, next) => {
     upload.array('files', 2)(req, res, (err) => {
         if (err){
             return res.status(400).json({ error: err.message });
         }
     })
-}
-router.get('/', function(req, res, next) {
-    res.render('addModel', {title: 'Add New Model'});
-});
+}*/
+//router.get('/', function(req, res, next) {
+    //res.render('addModel', {title: 'Add New Model'});
+//});
 
 //This endpoint needs to be completed to handle the uploading for the files to public/modelfiles
-router.post('/saveModel', (req, res) => {
+router.post('/saveModel',uploadMiddleware, (req, res) => {
     const name = req.body.name;
-    console.log(name);
+    //console.log(name);
     const modelDescription = req.body.modelDescription;
-    console.log(modelDescription);
+    //console.log(modelDescription);
     const objFile = req.files['objFileName'][0];
     const mtlFile = req.files['mtlFileName'][0];
     
-    const models = fs.readdirSync('./public/modelfiles/');
-    const fileIsPresent1 = models.includes(file1.name);
-    const fileIsPresent2 = models.includes(file2.name);
+    const models = fs.readdirSync('./public/modelfiles/admin/');
+    const fileIsPresent1 = models.includes(objFile.originalname);
+    const fileIsPresent2 = models.includes(mtlFile.originalname);
 
     // Check if the model already exists
     if (!fileIsPresent1 && !fileIsPresent2) {
@@ -53,6 +62,7 @@ router.post('/saveModel', (req, res) => {
         return res.redirect('/models');
     }
 
+    
     fs.renameSync(objFile.path, path.join(objFile.destination, name + path.extname(objFile.originalname)));
     fs.renameSync(mtlFile.path, path.join(mtlFile.destination, name + '_description' + path.extname(mtlFile.originalname)));
 
