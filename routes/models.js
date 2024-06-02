24dd2e40320b7c9c2e701fce69c49670a28ddb38
Pooja Prasad Rayapureddy <pj705379@dal.ca>
2024-06-02 17:20:37 -0300

    Implemented /searchModels endpoint, connected to frontend

--- routes/models.js
+++ routes/models.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET models page. */
 router.get('/', function(req, res, next) {
-    const modelfiles = './public/modelfiles/';
-
+    
     //Admin check
     let isAdmin = (req.signedCookies.admin == 'true');
 
     res.render('models', { title: 'Model Catalog', isAdmin: isAdmin});
 });
 
+router.get('/searchModels', function(req, res, next) {
+    const userSearch = req.query.search;
+    const modelFileCatalog = './public/catalog/modelFileCatalog.json';
+
+    const modelFileData = fs.readFileSync(modelFileCatalog);
+    const modelData = JSON.parse(modelFileData);
+    const allModels = Object.keys(modelData).map(key => modelData[key]);
+    const searchResults = allModels.filter(model => model.name.includes(userSearch));
+    
+    res.send(searchResults);
+});
+
 module.exports = router;
+
