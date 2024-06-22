d9b9393f5081dda940c9f8a11fcc3daba9d217ff
= <sh843173@dal.ca>
2024-06-22 19:41:10 -0300

    remove node-notifier from molecules page

--- routes/molecules.js
+++ routes/molecules.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-const notifier = require('node-notifier'); // Node Notifiers: https://www.npmjs.com/package/node-notifier
 
 router.get('/', function(req, res, next) {
     const molfiles = './public/molfiles/';
@@ -56,16 +55,11 @@ router.put('/editMolecule/:file', function(req, res) {
         // Write back to the JSON file
         fs.writeFileSync(catalogPath, JSON.stringify(parsedData));
 
-        // Notify success
-        notifier.notify({
-            title: 'Edit Successful',
-            message: `Molecule '${file}' updated successfully.`,
-        });
-
-        res.sendStatus(200); // Send success response
+        return res.send({ message: `Molecule '${file}' updated successfully.` });
     } catch(err) {
-        console.error(`Failed to update molecule file '${file}':`, err);
-        res.sendStatus(500); // Send error response
+        const errMsg = `Failed to update molecule file '${file}': ${err}`;
+        console.error(errMsg);
+        return res.status(500).send({ error: errMsg });
     }
 });
 
@@ -90,16 +84,11 @@ router.post('/deleteMolecule/:file', function(req, res) {
         // Write back to the JSON file
         fs.writeFileSync(catalogPath, JSON.stringify(parsedData));
 
-        // Notify success
-        notifier.notify({
-            title: 'Delete Successful',
-            message: `Molecule '${file}' deleted successfully.`,
-        });
-
-        res.sendStatus(200); // Send success response
+        return res.send({ message: `Molecule '${file}' deleted successfully.` });
     } catch(err) {
-        console.error(`Failed to delete molecule file '${file}':`, err);
-        res.sendStatus(500); // Send error response
+        const errMsg = `Failed to delete molecule file '${file}': ${err}`;
+        console.error(errMsg);
+        return res.status(500).send({ error: errMsg });
     }
 });
 
