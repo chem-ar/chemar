46353d7b7327aba8efc2d569bab433bad75ee0a5
Dhruvil Pechiwala <dhruvilpechiwala@Dhruvils-MacBook-Air.local>
2024-06-23 23:38:45 -0300

    Edit functionality working to be displayed on the table while changing the name and description

--- routes/models.js
+++ routes/models.js
@@ -3,18 +3,18 @@ var router = express.Router();
 var fs = require('fs');
 
 /* GET models page. */
-router.get('/', function(req, res, next) {
-    
+router.get('/', function (req, res, next) {
+
     //Admin check
     let isAdmin = (req.signedCookies.admin == 'true');
 
-    res.render('models', { title: 'Model Catalog', isAdmin: isAdmin});
+    res.render('models', { title: 'Model Catalog', isAdmin: isAdmin });
 });
 
-router.get('/searchModels', function(req, res, next) {
+router.get('/searchModels', function (req, res, next) {
     const userSearch = decodeURIComponent(req.query.search || '').toLowerCase();
 
-    if(!userSearch){
+    if (!userSearch) {
         return res.send([]);
     }
     const modelFileCatalog = './public/catalog/modelFileCatalog.json';
@@ -23,9 +23,32 @@ router.get('/searchModels', function(req, res, next) {
     const modelData = JSON.parse(modelFileData);
     const allModels = Object.keys(modelData).map(key => modelData[key]);
     const searchResults = allModels.filter(model => model.name.toLowerCase().includes(userSearch));
-    
+
     res.send(searchResults);
 });
 
-module.exports = router;
+router.post('/edit', function (req, res, next) {
+    const data = { ...req.body }
+    let id = req.query.id;
 
+    const modelFileCatalog = './public/catalog/modelFileCatalog.json';
+
+    const modelFileData = fs.readFileSync(modelFileCatalog);
+    const modelData = JSON.parse(modelFileData);
+
+    for (const key in modelData) {
+        if (modelData[key].id == parseInt(id)) {
+            modelData[key].name = data.name,
+                modelData[key].description = data.description
+            break;
+        }
+    }
+
+    var newModel = JSON.stringify(modelData);
+    fs.writeFileSync('./public/catalog/modelFileCatalog.json', newModel);
+
+    return res.redirect('/models');
+
+})
+
+module.exports = router;
