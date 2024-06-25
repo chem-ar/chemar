191e2a580e237c7b917fa5e2a66f61de0db8a843
Dhruvil Pechiwala <dhruvilpechiwala@Dhruvils-MacBook-Air.local>
2024-06-24 21:52:43 -0300

    Removing unnecessary logs and spacing in the code while adding comments

--- routes/addModel.js
+++ routes/addModel.js
@@ -22,12 +22,13 @@ const upload = multer({
 router.get('/', function (req, res, next) {
     res.render('addModel', { title: 'Add New Model' });
 });
-
+//To handle file uploading
 const uploadMiddleware = upload.fields([
     { name: 'objFileName', maxCount: 1 },
     { name: 'mtlFileName', maxCount: 1 }
 ]);
 
+//Creation of unique ID for different models which can be displayed in modelFileCatalog.json
 const uniqueId = (parsedData) => {
     if (parsedData.length == 0) {
         return 1;
@@ -43,38 +44,16 @@ router.post('/saveModel', uploadMiddleware, (req, res) => {
     const modelDescription = req.body.modelDescription;
     const objFile = req.files['objFileName'][0];
     const mtlFile = req.files['mtlFileName'][0];
-
-    console.log(objFile);
-    console.log(mtlFile);
-
-    
-
-    // const models = fs.readdirSync('./public/modelfiles/');
-    // const fileIsPresent1 = models.includes(objFile.originalname);
-    // const fileIsPresent2 = models.includes(mtlFile.originalname);
-
-    // Check if the model already exists
-    // if (fileIsPresent1 && fileIsPresent2) {
-    //     console.log('This model already exists.');
-    //     notifier.notify({
-    //         title: 'Save Unsuccessful.',
-    //         message: 'Cannot save file This model already exists.',
-    //     });
-    //     // Redirect back to the catalog page
-    //     return res.redirect('/models');
-    // }
     const newObjFileName = name + path.extname(objFile.originalname);
     const newMtlFileName = name + path.extname(mtlFile.originalname);
 
     fs.renameSync(objFile.path, path.join(objFile.destination, newObjFileName));
     fs.renameSync(mtlFile.path, path.join(mtlFile.destination, newMtlFileName));
 
-    console.log("hello");
-
     // Read modelFileCatalog json file and add info to it
     var rawdata = fs.readFileSync('./public/catalog/modelFileCatalog.json');
     var parsedData = JSON.parse(rawdata);
-
+    // To check whether the model is present or not 
     for(const key in parsedData) {
         if(parsedData[key].name == req.body.name){
             if(parsedData[key].description == modelDescription){
@@ -85,10 +64,6 @@ router.post('/saveModel', uploadMiddleware, (req, res) => {
 
     // Variables to be added to modelFileCatalog
     var modelName = req.body.name;
-
-
-
-
     obj = {
         id: uniqueId(parsedData),
         name: modelName,
