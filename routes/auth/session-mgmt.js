394f08b91cee13752f2d0fe35ec8bb8baafea76e
Ferin <miyani>
2024-07-03 02:59:56 -0300

    implemented start end and check session

--- routes/auth/session-mgmt.js
+++ routes/auth/session-mgmt.js
@@ -1,15 +1,26 @@
-export function startSession(res) {
+let key = "";
+
+function startSession(res) {
     // TODO store a randomly generated session token to the user's cookies 
+    let token = globalThis.crypto.randomUUID()
+    key = token
+    res.cookie('boss', token)
     // using res. Save this session token somewhere on the server too
 }
 
-export function checkSession(req) {
+function checkSession(req) {
     // TODO check cookie in req. It should match a session token stored 
     // somewhere on the server. Return true if the user is logged in
-    return false;
+    const token = req.cookies.boss;
+    return token === key;
 }
 
-export function endSession(res) {
+function endSession(res) {
     // TODO delete the session token stored in the user's cookies and on the 
     // server
+    res.clearCookie('boss');
+    key = ""
 }
+
+
+module.exports = { startSession, checkSession, endSession }
