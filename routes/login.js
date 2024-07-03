20af599329b2792ad77af6f9ee2db27a20ce86fb
Ferin <miyani>
2024-07-03 02:59:56 -0300

    implemented start end and check session

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var { startSession } = require('./auth/session-mgmt')
 var fs = require('fs');
 
 router.get('/', function(req, res, next) {
     //Admin check
-    if (req.signedCookies.admin == 'true') res.redirect('/');
+    // if (req.signedCookies.admin == 'true') res.redirect('/');
+    // startSession(res)
     res.render('login', {title: 'Login Page'});
 });
 
