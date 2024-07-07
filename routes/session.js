24dfd5ed9db1cfccecf0a7b0e36a1dd40000f871
= <sh843173@dal.ca>
2024-07-07 19:40:49 -0300

    refactor, remove dead code

--- routes/session.js
+++ routes/session.js
@@ -4,27 +4,27 @@ var fs = require('fs');
 const { startSession, checkSession } = require('./auth/session-mgmt')
 const bcrypt = require('bcrypt')
 
-async function handler(req, res, next) {
+//Create cookie here then redirect
+router.get('/', function(req, res) {
+  res.redirect("/");
+});
+
+router.post('/', async function (req, res) {
   let adminPass = JSON.parse(fs.readFileSync("./routes/auth/admin.json")).admin.password;
   const hash = await bcrypt.compare(req.body.password, adminPass)
-  console.log(hash);
   if(hash){
     startSession(req, res)
     return res.status(200).send({});
   }
   return res.status(401).send({error: "Password Incorrect"});
-}
+});
 
-async function forgotHandler(req, res, next) {
+router.post('/forgot', async function(req, res) {
   let adminPass = JSON.parse(fs.readFileSync("./routes/auth/admin.json"));
   let isAdmin = checkSession(req)
-  console.log("about to check");
   const hash = await bcrypt.compare(req.body.password, adminPass.admin.password)
-  console.log(hash);
   if(isAdmin && hash){
-    console.log("about to hash");
     let newPassword = await bcrypt.hash(req.body.newPassword, 5)
-    console.log("new password after hash");
     adminPass.admin.password = newPassword
     let arr = JSON.stringify(adminPass)
     fs.writeFileSync("./routes/auth/admin.json", arr)
@@ -33,14 +33,6 @@ async function forgotHandler(req, res, next) {
   else{
     return res.status(401).json({error: "Invalid Credentials."})
   }
-}
-
-//Create cookie here then redirect
-router.get('/', function(req, res, next) {
-  res.redirect("/");
 });
 
-router.post('/', handler);
-
-router.post('/forgot', forgotHandler);
 module.exports = router;
