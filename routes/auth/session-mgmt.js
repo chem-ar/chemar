b073162212d5d44ac3691032643c843266eb24bc
Ferin <miyani>
2024-07-07 02:50:00 -0300

    impemented startsession and checksession by storing the sessionsin database and allowinng multiple device login functionality

--- routes/auth/session-mgmt.js
+++ routes/auth/session-mgmt.js
@@ -1,9 +1,21 @@
+var fs = require('fs')
 let key = "";
 
 function startSession(res) {
     // TODO store a randomly generated session token to the user's cookies 
     let token = globalThis.crypto.randomUUID()
-    key = token
+    // key = 
+    let sess = {
+        token: token,
+        time: Date.now()
+    }
+
+    let ad = fs.readFileSync('./admin.json');
+    let adminData = JSON.parse(ad)
+    adminData.admin.session.push(sess)
+
+    fs.writeFileSync('./admin.json', JSON.stringify(adminData))
+    
     res.cookie('boss', token)
     // using res. Save this session token somewhere on the server too
 }
@@ -12,14 +24,25 @@ function checkSession(req) {
     // TODO check cookie in req. It should match a session token stored 
     // somewhere on the server. Return true if the user is logged in
     const token = req.cookies.boss;
-    return token === key;
+    let exists = false
+
+    let ad = fs.readFileSync('./admin.json')
+    let adminData = JSON.parse(ad)
+
+    adminData.admin.session.map((e) => {
+        if(e.token === token){
+            e.time = Date.now()
+            return exists = true
+        }
+    })
+
+    return exists;
 }
 
 function endSession(res) {
     // TODO delete the session token stored in the user's cookies and on the 
     // server
     res.clearCookie('boss');
-    key = ""
 }
 
 
