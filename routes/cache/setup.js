cea24bb46185e964974e9bb7f8d15a595ff996e7
Branden McInnis <br915063@dal.ca>
2024-07-17 18:00:10 -0300

    added safety checks if required folders were missing

--- routes/cache/setup.js
+++ routes/cache/setup.js
@@ -1,21 +1,40 @@
 var fs = require('fs'); 
 
 function initializeCache(){
-    let molfileCatalogExists = fs.existsSync('./public/catalog/molfileCatalog.json ');
-    let modelFileCatalog = fs.existsSync('./public/catalog/modelFileCatalog.json ')
-    let sceneCatalog = fs.existsSync('./public/catalog/sceneCatalog.json ');
+    let molFilesExists = fs.existsSync('./public/molFiles');
+    let scenesExists = fs.existsSync('./public/ scenes');
+    let modelFilesExists = fs.existsSync('./public/modelFiles');
+ 
+    let molfiles= fs.existsSync('./public/catalog/molfileCatalog.json');
+    let modelFiles= fs.existsSync('./public/catalog/modelFileCatalog.json ')
+    let scenes = fs.existsSync('./public/catalog/sceneCatalog.json ');
 
-    if(!molfileCatalogExists){
+
+    if(!molFilesExists){
+        fs.mkdirSync('./public/molFiles', {recursive: true});
+    }
+   
+    if(!scenesExists){
+        fs.mkdirSync('./public/scenes', {recursive: true});
+    }
+
+    if(!modelFilesExists){
+        fs.mkdirSync('./public/modelFiles', {recursive: true});
+    }
+
+
+
+    if(!molfiles){
         fs.writeFileSync("./public/catalog/molfileCatalog.json", "{}");
     }
 
-    if(!modelFileCatalog){
+    if(!modelFiles){
         fs.writeFileSync("./public/catalog/modelFileCatalog.json", "[]");
     }
     
-    if(!sceneCatalog){
+    if(!scenes){
         fs.writeFileSync("./public/catalog/sceneCatalog.json", "{}");
-    }  
+    } 
 }
 
 module.exports = initializeCache;
