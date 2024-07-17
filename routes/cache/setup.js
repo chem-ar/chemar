690b264190eff353807221371a216bbfaa354e9d
Branden McInnis <br915063@dal.ca>
2024-07-17 14:58:29 -0300

    created function initializeCache to handle cases where Catalogs are not populated

---
+++ routes/cache/setup.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs'); 
+
+
+function initializeCache() {
+    let molfileCatalogExists = fs.existsSync('./public/catalog/molfileCatalog.json ');
+    let modelFileCatalog = fs.existsSync('./public/catalog/modelFileCatalog.json ')
+    let sceneCatalog = fs.existsSync('./public/catalog/sceneCatalog.json ');
+
+    if(!molfileCatalogExists){
+        fs.writeFileSync("./public/catalog/molfileCatalog.json", "{}");
+    }
+
+    if(!modelFileCatalog){
+        fs.writeFileSync("./public/catalog/modelFileCatalog.json", "[]");
+    }
+
+    if(!sceneCatalog){
+        fs.writeFileSync("./public/catalog/sceneCatalog.json", "{}");
+    }  
+
+}
+
+module.exports = router;
