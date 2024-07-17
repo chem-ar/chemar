89b24f86da4e0f9bd7eba3bc091b2dcef27f52e5
Branden McInnis <br915063@dal.ca>
2024-07-17 16:40:20 -0300

    initcache is no longer a router

--- routes/cache/setup.js
+++ routes/cache/setup.js
@@ -1,9 +1,6 @@
-var express = require('express');
-var router = express.Router();
 var fs = require('fs'); 
 
-
-router.get('/', function initializeCache(req, res, next){
+function initializeCache(){
     let molfileCatalogExists = fs.existsSync('./public/catalog/molfileCatalog.json ');
     let modelFileCatalog = fs.existsSync('./public/catalog/modelFileCatalog.json ')
     let sceneCatalog = fs.existsSync('./public/catalog/sceneCatalog.json ');
@@ -19,6 +16,6 @@ router.get('/', function initializeCache(req, res, next){
     if(!sceneCatalog){
         fs.writeFileSync("./public/catalog/sceneCatalog.json", "{}");
     }  
-});
+}
 
-module.exports = router;
+module.exports = initializeCache;
