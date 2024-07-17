3371856921f4d406ec9f6eb91a286e43ac043acf
Teerth <tr633364@dal.ca>
2024-07-17 03:15:18 -0300

    UI theme of the application update, fix the overflow of the Tables in Mobile View

--- public/js/scenes/scene-mgmt.js
+++ public/js/scenes/scene-mgmt.js
@@ -21,20 +21,6 @@ export function initScene(pngFile, markerData, threeArea, window){
     
     scene1.add( camera );
 
-    let spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set(100, 1000, 100);
-
-    spotLight.castShadow = true;
-
-    spotLight.shadow.mapSize.width = 1024;
-    spotLight.shadow.mapSize.height = 1024;
-
-    spotLight.shadow.camera.near = 500;
-    spotLight.shadow.camera.far = 4000;
-    spotLight.shadow.camera.fov = 30;
-
-    scene1.add(spotLight);
-
     renderer = new THREE.WebGLRenderer({ alpha: true });
     renderer.setClearColor( 0x000000, 0 );
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -152,26 +138,4 @@ export function exportSceneData() {
     const { position, rotation: { x, y, z }, scale } = markerPlane;
     const markerData = { position, rotation: { x, y, z }, scale };
     return { markerData, sceneMolecules };
-}
-
-/**
- * In the scene viewer, molecules are rendered relative to the marker, unlike 
- * the scene editor where everything is relative some origin (0, 0, 0). As a 
- * result, molecules need to be adjusted using this function.
- * 
- * The consequences of this are:
- * - The Y and Z axes need to be swapped (Y = Z, Z = -Y)
- * - Molecules need to be rotated (they are facing the floor instead of 
- *   facing us)
- * - Molecules' positions need to be relative to the marker, not the origin
- * 
- * @returns void. The passed molecule is changed in place
- */
-export function adjustMolToMarker(mol, marker) {
-    [mol.position.y, mol.position.z] = [mol.position.z, -mol.position.y];
-    mol.rotateX(Math.PI / 2);
-
-    mol.position.x -= Number(marker.position.x);
-    mol.position.y -= Number(marker.position.z);
-    mol.position.z += Number(marker.position.y);
 }
--- routes/models.js
+++ routes/models.js
@@ -47,31 +47,21 @@ router.post('/edit', function (req, res, next) {
                 exists = true;
             }
         }
-        
     }
     if(!exists){
-        const previousName = modelData[n].name;
         modelData[n].description = data.description
         modelData[n].name = data.name;
-        modelData[n].files = {
-            obj: data.name + "-"+ id + ".obj",
-            mtl: data.name + "-"+ id + ".mtl"
-        }
-        fs.renameSync(`./public/modelfiles/${previousName + "-"+ id}.mtl`, `./public/modelfiles/${data.name + "-"+ id}.mtl`);
-        fs.renameSync(`./public/modelfiles/${previousName + "-"+ id}.obj`, `./public/modelfiles/${data.name + "-"+ id}.obj`);
-    }
-    else{
-        return res.status(409).send('')
     }
     var newModel = JSON.stringify(modelData);
     fs.writeFileSync('./public/catalog/modelFileCatalog.json', newModel);
     return res.redirect('/models');
+
 })
 
 //Handling the delete functionality
 router.get('/delete', function (req, res, next) {
-    //return res.status(500).send({});
     const id = req.query.id
+
     const modelFileCatalog = './public/catalog/modelFileCatalog.json';
 
     const modelFileData = fs.readFileSync(modelFileCatalog);
@@ -84,11 +74,16 @@ router.get('/delete', function (req, res, next) {
             n = key
         }
     }
+
     const modelDelete = modelData[n];
 
     // Deleting the .mtl and .obj files from modelfiles folder once the deletion functionality is used
-    fs.unlinkSync(`./public/modelfiles/${modelDelete.files.mtl}`);
-    fs.unlinkSync(`./public/modelfiles/${modelDelete.files.obj}`);
+    try {
+        fs.unlinkSync(`./public/modelfiles/${modelDelete.name}.mtl`);
+        fs.unlinkSync(`./public/modelfiles/${modelDelete.name}.obj`);
+    } catch (err) {
+        console.error(err);
+    }
 
     //To remove the model when deleted from array
     modelData.splice(n, 1)
