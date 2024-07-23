0d44acb4db0a4b10a0800fd8b8f5474a92e5fc94
= <sh843173@dal.ca>
2024-07-23 16:15:07 -0300

    import express-async-errors pkg

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var https = require("https");
 
 var createError = require("http-errors");
 var express = require("express");
+require('express-async-errors');
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
