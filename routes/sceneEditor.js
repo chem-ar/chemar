0b4fb68036e5146903b8940267988e0099ae9ce5
Pooja Prasad Rayapureddy <pj705379@dal.ca>
2024-07-31 12:23:45 -0300

    Refactoring: added a change to sceneEditor and deleted admin.json

--- routes/sceneEditor.js
+++ routes/sceneEditor.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs'); 
+const { checkSession } = require('./auth/session-mgmt');
 const path = require('path');
 router.get('/', function(req, res, next) {
   const scenes = './public/scenes/'
