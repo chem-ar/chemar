d03132e3b3db1fcda2a99237181f8fb7d594c42b
Ferin <miyani>
2024-07-30 22:18:19 -0300

    changed response

--- routes/allAdmin.js
+++ routes/allAdmin.js
@@ -7,7 +7,7 @@ var fs = require('fs')
 router.get('/alladmin', async function (req, res, next) {
     let {isAdmin, isowner} = checkSession(req, res);
     if (!isowner) return res.status(401).json({error: 'Please log in as owner of the page'});
-    res.render('allAdmins', { title: 'allAdmin', isowner })
+    res.redirect('allAdmins', { title: 'allAdmin', isowner })
 })
 
 router.get('/alladminsearch', async function (req, res, next) {
