fcf019bf1a5f510dfd7ac67a4cf51a1004caefa6
Ferin <miyani>
2024-07-30 22:36:37 -0300

    redirect was not working so had to render it again

--- routes/allAdmin.js
+++ routes/allAdmin.js
@@ -21,7 +21,7 @@ function emailExists(email){
 router.get('/alladmin', async function (req, res, next) {
     let {isAdmin, isowner} = checkSession(req, res);
     if (!isowner) return res.status(401).json({error: 'Please log in as owner of the page'});
-    res.redirect('allAdmins', { title: 'allAdmin', isowner })
+    res.render('allAdmins', { title: 'allAdmin', isowner })
 })
 
 router.get('/alladminsearch', async function (req, res, next) {
