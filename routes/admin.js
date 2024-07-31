10fbe641a1a1d2c952053b8e141ca517dd5e80f8
Pooja Prasad Rayapureddy <pj705379@dal.ca>
2024-07-31 12:51:40 -0300

    Resolving threads on admin-scale

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var { checkSession } = require('./auth/session-mgmt')
-const bcrypt = require('bcrypt');
 
 /* GET admin page. */
 router.get('/', function (req, res, next) {
