33c2d29161a7c18557aa11f7a5daa63e118ca736
Shahriar Hossain <shahriar.hossain@dal.ca>
2024-10-11 14:29:14 -0300

    Created personalized url for password reset form

--- routes/passwordReset.js
+++ routes/passwordReset.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+const { startSession, checkSession, endSession } = require('./auth/session-mgmt')
 
-router.get('/', function(req, res, next) {
-   
+router.get('/', function (req, res) {
+    req.cookies.session = req.query.token;
+    req.body.email = req.query.email;
+    let confirm = false;
 
-    res.render('passwordReset', {title: 'Password Reset Page'});
+    let { isAdmin, isowner } = checkSession(req, res);
+    if (isAdmin) {
+        confirm = true;
+        endSession(req, res)
+    }
+
+    if (confirm) {
+        return res.render('passwordReset', { title: 'Password Reset Page' });
+    } else {
+        return res.status(404);
+    }
+
 });
 
 module.exports = router;
