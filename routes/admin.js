388b8800ae807bb87a0872813723fcc7033201df
Ally MacKenzie <ally.mackenzie@dal.ca>
2025-03-19 11:36:15 -0300

    feat(multi user support): implement RBAC for molecules, admin, and scenes pages

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,20 @@ var router = express.Router();
 var { checkSession } = require('./auth/session-mgmt');
 
 router.get('/', async function (req, res, next) {
-    let { isAdmin, isOwner } = await checkSession(req, res);
+    let {isAdmin, isInstructor, isOwner} = checkSession(req, res);
+    let userRole = res.locals.userRole;
+    if(userRole === 'instructor'){
+        isAdmin = true;
+    }else if(userRole === 'admin'){
+        isAdmin = true;
+        isOwner = true;
+    }
     
     if (!isAdmin) {
         return res.redirect("/");
     }
 
-    res.render('admin', { title: 'Admin', isAdmin, isOwner });
+    res.render('admin', { title: 'Admin', isAdmin, isOwner, isInstructor });
 });
 
 module.exports = router;
